5fa55bf792a2289e23568db046914d3c69dbae51
nico <nico.marcq@gmail.com>
2015-08-30 12:06:25 +0200

    change name of the project

--- webgui/tasks.py
+++ webgui/tasks.py
@@ -5,3 +5,8 @@ from celery import shared_task
 @shared_task
 def start_pump_task(id):
     print "Call twisted client"
+
+
+@shared_task
+def make_cocktail_task(info):
+    print "Call twisted client"
