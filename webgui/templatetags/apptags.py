066e4418a037dd69351c2d0a4cbec67269855470
nico <nico.marcq@gmail.com>
2015-08-30 17:02:45 +0200

    fix (again) the view. Cocktail content now appear correctly

--- webgui/templatetags/apptags.py
+++ webgui/templatetags/apptags.py
@@ -1,10 +1,15 @@
 from django import template
 register = template.Library()
+from webgui.models import Cocktailinfo
 
 
-@register.filter
-def custom_m2m(queryset, forloop_counter):
-    try:
-        return queryset[forloop_counter].volume
-    except IndexError:
-        pass
+@register.simple_tag
+def custom_m2m(bottle_id, cocktail_id):
+    """
+    Used to get volume information from A cocktail and a bottle
+    :param bottle_id:
+    :param cocktail_id:
+    :return:
+    """
+    info = Cocktailinfo.objects.get(bottle=bottle_id, cocktail=cocktail_id)
+    return info.volume
