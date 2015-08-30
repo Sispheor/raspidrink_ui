77fc089852533e6f3545916b70f165d058194b42
nico <nico.marcq@gmail.com>
2015-08-30 12:05:41 +0200

    add RPi url in settings

---
+++ utils/get_highter.py
@@ -0,0 +1,10 @@
+def get_highter_volume(slot_volume_dict):
+        """
+        Return the bigger integer in the list
+        :param slot_volume_dict:
+        :return:
+        """
+        list_volume = []
+        for el in slot_volume_dict:
+            list_volume.append(int(el['volume']))
+        return max(list_volume)
