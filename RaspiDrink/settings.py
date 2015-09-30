36e168bd97f773792c28705bdce16fdea8692f75
nico <nico.marcq@gmail.com>
2015-09-30 19:50:34 +0200

    remove celery settings

--- RaspiDrink/settings.py
+++ RaspiDrink/settings.py
@@ -111,13 +111,6 @@ STATICFILES_FINDERS = (
     'dajaxice.finders.DajaxiceFinder',
 )
 
-# CELERY SETTINGS
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-
 # RAPI IP
 RPI_IP = '192.168.0.22'
 
