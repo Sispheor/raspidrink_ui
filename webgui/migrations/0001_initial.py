89c415a380a7b3f292f38e05a7d2ab53d2ce6847
nico <nico.marcq@gmail.com>
2015-10-16 23:44:40 +0200

    add initial database schema

---
+++ webgui/migrations/0001_initial.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Bottle',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('slot', models.IntegerField()),
+                ('is_present', models.BooleanField(default=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Cocktail',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('lock', models.BooleanField(default=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Cocktailinfo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('volume', models.IntegerField()),
+                ('bottle', models.ForeignKey(to='webgui.Bottle')),
+                ('cocktail', models.ForeignKey(to='webgui.Cocktail')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='cocktail',
+            name='bottles',
+            field=models.ManyToManyField(to='webgui.Bottle', through='webgui.Cocktailinfo'),
+            preserve_default=True,
+        ),
+    ]
